4266f3afb906f867623adb14665ed9590d71e8e5
Mohanapriya S <mohanapriya.s@kgisl.com>
2019-07-29 17:18:59 +0530

    heroku included

--- src/main/java/com/kgisl/travis1/controller/StudentController.java
+++ src/main/java/com/kgisl/travis1/controller/StudentController.java
@@ -8,6 +8,7 @@ import com.kgisl.travis1.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * StudentController
  */
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/student")
 public class StudentController {
--- src/main/resources/static/student.js
+++ src/main/resources/static/student.js
@@ -1,16 +1,33 @@
 var app=new function(){
+        var student="";
 
     this.fetchall=function(){
-        var xhttp = new XMLHttpRequest();
+        var xhttp = new XMLHttpRequest();        
  xhttp.onreadystatechange = function () {
  
  if (this.readyState == 4 && this.status == 200) {
- countries = JSON.parse(xhttp.responseText);
+ student = JSON.parse(xhttp.responseText);
  app.printtable();
  }
  };
- xhttp.open("GET", "http://zigwheels.herokuapp.com/api/teams/", true);
+ xhttp.open("GET", "https://travis1.herokuapp.com/student/", true);
  xhttp.send();
  };
 
-    };
+ this.printtable = function () {
+        // console.log(student);
+        var data = '';
+        for (var i in student) {
+        data += '<tr>';
+        data += '<td>' + student[i].id + '</td>';
+        data += '<td>' + student[i].name + '</td>';
+        data += '<td><button onclick="app.Edit(' + student[i].id + ')">Edit</button></td>';
+        data += '<td><button onclick="app.Delete(' + student[i].name + ')">Delete</button></td>';
+        data += '</tr>';
+        data += '<br>';
+        data += '<br>';
+        }
+        document.getElementById("student").innerHTML = data;
+}
+
+    }; 
