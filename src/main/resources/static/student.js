a4e6fe847e94bd463d6435e9d54dd8c0e35f347c
Mohanapriya S <mohanapriya.s@kgisl.com>
2019-07-30 09:23:13 +0530

    save or update

--- src/main/resources/static/student.js
+++ src/main/resources/static/student.js
@@ -1,8 +1,8 @@
 var app=new function(){
-        var student="";
-
+    var student=[];
+    var mod="";
     this.fetchall=function(){
-        var xhttp = new XMLHttpRequest();        
+        var xhttp = new XMLHttpRequest();
  xhttp.onreadystatechange = function () {
  
  if (this.readyState == 4 && this.status == 200) {
@@ -13,7 +13,6 @@ var app=new function(){
  xhttp.open("GET", "https://travis1.herokuapp.com/student/", true);
  xhttp.send();
  };
-
  this.printtable = function () {
         // console.log(student);
         var data = '';
@@ -28,6 +27,64 @@ var app=new function(){
         data += '<br>';
         }
         document.getElementById("student").innerHTML = data;
-}
+        };
+        
+        this.Edit = function (id) {
+        mode = "edit";
+        document.getElementById('btn').innerHTML = "Update";
+        for (var i = 0; i < student.length; i++) {
+        
+        if (student[i].id == id) {
+        // alert(id);
+        document.getElementById('id').value = student[i].id;
+        document.getElementById('name').value = student[i].name;
+        }
+        }
+        };
 
-    }; 
+        this.saveorupdate = function () {
+                var id = document.getElementById('id').value;
+                var name = document.getElementById('name').value;
+                if (mode == "") {
+                alert("save called");
+                // {"id": 0, "name": "TeamIndia"}
+                var newStudent = {"id": 0, "name": name};
+                alert(newStudent);
+                var url = "https://travis1.herokuapp.com/student/";
+                var xhr = new XMLHttpRequest();
+                xhr.onload = function () {
+                if (xhr.readyState == 4 && xhr.status == "200") {
+                app.FetchAll();
+                }
+                }
+                xhr.open("POST", url , true);
+                xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+                xhr.send(JSON.stringify(newStudent));
+                app.FetchAll();
+                }
+                else {
+                alert("update called");
+                // alert("id" + id + "name " + name);
+                var updateStudent = {"id": 0, "name": name};
+                alert(updateStudent);
+                var url = "https://travis1.herokuapp.com/student/";
+                var xhr = new XMLHttpRequest();
+                xhr.onload = function () {
+                if (xhr.readyState == 4 && xhr.status == "200") {
+                app.FetchAll();
+                }
+                }
+                xhr.open("PUT", url, true);
+                xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+                xhr.send(JSON.stringify(updateStudent));
+                document.getElementById('btn').innerHTML = "Add";
+                mode = "";
+                app.FetchAll();
+                }
+                
+                document.getElementById('id').value = "";
+                document.getElementById('name').value = "";
+                };
+               
+
+    };
